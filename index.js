f74c4caa425ddc82344da5565e710f9fbf2c361e
unknown <aminpyss@example.com>
2025-09-08 12:21:26 +0330

    starting files

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import axios from 'axios';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.listen(port, () => {
+    `listenint to port: ${port}`
+});
