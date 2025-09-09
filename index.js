6e63b46bfd1fc8dcadee81e18a0612e04e5db372
unknown <aminpyss@example.com>
2025-09-09 23:49:54 +0330

    front: header

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ const port = 3000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
 
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+});
+
 app.listen(port, () => {
     `listenint to port: ${port}`
 });
