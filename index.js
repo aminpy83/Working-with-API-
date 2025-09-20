3ac6be2b060d2987f8b091370116dc7664bc0e69
unknown <aminpyss@example.com>
2025-09-21 00:29:32 +0330

    still on jokeAPI

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
+const jokeUrl = 'https://v2.jokeapi.dev/joke';
+
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -11,6 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.get('/', (req, res) => {
     res.render('index.ejs');
 });
+
+
+
+
+
+
 
 app.listen(port, () => {
     `listenint to port: ${port}`
