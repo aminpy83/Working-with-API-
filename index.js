059c85385578eea6ab114d37f7cdd0c644bca48c
unknown <aminpyss@example.com>
2025-09-23 10:55:42 +0330

    end of joke api task

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3000;
-const jokeUrl = 'https://v2.jokeapi.dev/joke';
+const jokeUrl = 'https://v2.jokeapi.dev/joke/';
 
 
 app.use(express.static('public'));
@@ -14,7 +14,30 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+app.post('/joke', async (req, res) => {
+    const language = req.body['language'];
+    const category = req.body['category'];
+    console.log(language, category);
 
+    try{
+        switch (language) {
+            case 'en':
+                var joke = await axios.get( jokeUrl + category);
+            default:
+                var joke = await axios.get( jokeUrl + category +'?lang=' + language);
+        };
+			console.log(joke.data)
+			if (joke.data.joke){
+            	var mainJoke = joke.data.joke;
+			} else {
+				var mainJoke = joke.data.setup + joke.data.delivery;
+			};
+            res.render('index.ejs', {data: mainJoke})
+    } catch (err) {
+        console.log('error:', err);
+		res.redirect('/', {e: 'NO SUCH FOUND!'});
+    }
+})
 
 
 
