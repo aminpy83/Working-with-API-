3dcdc2ac9882153ec0a77d00f9e54f35cc883378
unknown <aminpyss@example.com>
2025-09-25 23:02:00 +0330

    weather api done

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 const jokeUrl = 'https://v2.jokeapi.dev/joke/';
-
+// const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${weatherapi}`;
+const geoApi = 'e9c6c17ae1785e22f52adb763b13ce51';
+const weatherapi = 'd66feba840a248e39d8110731252509';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -35,11 +37,44 @@ app.post('/joke', async (req, res) => {
             res.render('index.ejs', {data: mainJoke})
     } catch (err) {
         console.log('error:', err);
-		res.redirect('/', {e: 'NO SUCH FOUND!'});
+		res.render('index.ejs', {e: 'NO SUCH FOUND!'});
     }
 })
 
+app.post('/weather', async (req, res) => {
+    try {
+        const data = req.body;
+        const name = data['name'];
+        const code = data['code'];
+        const geocode = await axios.get(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${name},${code}&limit=1&appid=${geoApi}`);
+        // console.log(geocode);
+        // res.redirect('/');
+        console.log(geocode.data);
+        var lat = geocode.data[0].lat;
+        var lon = geocode.data[0].lon;
+        console.log('first try worked', lat, lon);
 
+    } catch (error) {
+        console.log('first try error : ',error);
+        res.render('index.ejs', {geoErr: 'wrong input'});
+    }
+
+    try {
+        const weatherUrl = await axios.get(
+            `http://api.weatherapi.com/v1/forecast.json?key=${weatherapi}&q=${lat},${lon}&days=1&aqi=no&alerts=no`);
+
+        console.log(weatherUrl.data.forecast.forecastday[0].day.condition);
+        const status = weatherUrl.data.forecast.forecastday[0].day.condition
+         const picture = 'https:' + status.icon;
+        res.render('index.ejs', {stat: status.text , picture: picture});
+
+    } catch (error) {
+        console.log('second try error : ', error);
+        res.render('index.ejs', {weatherError: 'reached the limit'})
+    }
+    
+})
 
 
 
